880904734b64a467601721d417fca47a1632ee3d
cyhed <zolotovv453@gmail.com>
2023-03-30 23:50:45 +0300

    кодировшик готов

--- OZI_Hamming-code/Program.cs
+++ OZI_Hamming-code/Program.cs
@@ -11,61 +11,89 @@ namespace OZI_Hamming_code
     {
         static void Main(string[] args)
         {
-            HammingCode.Coding();
+            HammingCode.Coding('䐽');            
         }
     }
 
     class HammingCode
-    {
-        public static int BaseBitSequenceLength = 13;
-        public static string Coding()
+    {        
+        public static string Coding(char symbol,int baseBitSequenceLength = 16)
         {
-            string bits = CharToBits('л');
+            string bits = CharToBits(symbol, baseBitSequenceLength);            
             Console.WriteLine(bits);
-            AddEmptyControlBits(bits);
+            bits = AddEmptyControlBits(bits);
+            Console.WriteLine(bits);
+            int[] posBit = BitLocations(bits.Length);
+
+            StringBuilder sb = new StringBuilder(bits);
+            foreach (var controlBit in posBit)
+            {
+                char CB = ControlBitCalculation(controlBit, bits);
+                Console.WriteLine(CB);
+                sb[controlBit - 1] = CB;
+            }
+            bits = sb.ToString();
+            Console.WriteLine(bits);
+
             return "-1";
         }
         public static string Decoding()
         {
             return "-1";
         }
-        //добавляет на нужные места битовой последовательности контрольные биты, котороые пока не несут никакой информации, а именно 0
+        //добавляет на нужные места битовой последовательности контрольные биты,
         public static string AddEmptyControlBits(string bits)
         {
             int bitLen = bits.Length;
-            int[] numControlBits = HowManyControlBitspublic(bits.Length);
-            int numControlBitsLen = numControlBits.Length;
-            Console.WriteLine("[{0}]", string.Join(", ", numControlBits));
-
+            int[] numControlBits = BitLocations(bits.Length);
+            int numControlBitsLen = numControlBits.Length;   
             int bitNum = 0;
             int newBitsLen = bitLen + numControlBitsLen;
-            Console.WriteLine("bitLen");
+            
             char[] bitsWithControl= new char[newBitsLen];
             for(int strPos=0; strPos < newBitsLen; strPos++)
             {
                 //если номер сивола строки совпадает с позицией контрольного бита под номером bitNum ,вставить пустой контрольный бита
                 if (bitNum< numControlBitsLen && numControlBits[bitNum] == strPos+1)
                 {
-                    bitsWithControl[strPos] = '0';
-                    Console.Write("ctrl ");
-                    Console.WriteLine("[{0}]", string.Join(", ", bitsWithControl));
+                    bitsWithControl[strPos] = '0';                    
                     //увеличить номер  контрольного бита
                     bitNum++;                                       
                 }
                 else
-                {
-                    Console.WriteLine($"    strPos={strPos}; strPos({strPos}) - bitNum({bitNum})+ ={strPos - bitNum}<- {bits.Length}");
-                    bitsWithControl[strPos] = bits[strPos - bitNum];
-                    Console.Write("def  ");
-                    Console.WriteLine("[{0}]", string.Join(", ", bitsWithControl));
+                {                    
+                    bitsWithControl[strPos] = bits[strPos - bitNum];                    
                 }
                 
-            }
-            Console.WriteLine(bitsWithControl.Length);
+            }           
             return string.Concat(bitsWithControl);
         }
+        //cчитает количество бит равных 1 в группе, если их четное кол-во,то возвоащает '0'
+        public static char ControlBitCalculation(int bitNumber,string bits)
+        {
+            int contOne=0;
+            for (int groupPos = bitNumber; groupPos <= bits.Length; groupPos= groupPos+ bitNumber * 2)
+            {
+                for(int globalPos = groupPos; globalPos < groupPos+ bitNumber; globalPos++)
+                {
+                    if (globalPos <= bits.Length)
+                    {
+                        Console.WriteLine($"{bits[globalPos - 1]} -- {globalPos}    {groupPos}");
+                        if (bits[globalPos - 1] == '1')
+                        {
+                            contOne++;
+                        }
+                        
+                    }
+                }
+            }            
+            if (0 == contOne % 2) 
+                return '0';
+            else 
+                return '1' ;
+        }
         //считает в каких местах должны быть контрольные биты
-        public static int[] HowManyControlBitspublic(int wordLength)
+        public static int[] BitLocations(int wordLength)
         {
             int powTwo = 0;
             
@@ -74,22 +102,21 @@ namespace OZI_Hamming_code
             {
                 powTwo = (int)Math.Pow(2, power);
                 // если убрать то запишется 1 лишняя степень 2
-                if (powTwo< wordLength)
-                    controlBitsPos.Add(powTwo);
-                Console.WriteLine($"power {power}  powTwo {powTwo} wordLength{wordLength}");                
+                if (powTwo<= wordLength)
+                    controlBitsPos.Add(powTwo);                                
             }
             
             return controlBitsPos.ToArray();
         }
         //производит перевод символа в битовую последовательность
-        public static string CharToBits(char symbol)
+        public static string CharToBits(char symbol,int baseBitSequenceLength)
         {
             string bit = Convert.ToString((int)symbol, 2);
+            Console.WriteLine((int)symbol);
             //так как длинна битовой последовательности для каждого символа разная, приводим
             //её к единому значению,если бит-последовательность занимает более 16, измените число            
-            while (bit.Length < HammingCode.BaseBitSequenceLength)
-                bit = '0' + bit;
-            Console.WriteLine(bit.Length);
+            while (bit.Length < baseBitSequenceLength)
+                bit = '0' + bit;            
             return bit;
         }
         //производит перевод битовой последовательности в символ
